fb8ca49b51d2a8817f79a8540d4b6ab699c0d0e0
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-12 18:08:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,23 @@
 class Dog
-def initialize(name, breed = "Mutt")
-  
+def initialize(name, breed)
+  @name = name
+  @breed = breed
+end
+
+#setter methods
+def name=(name)
+  @name = name
+end
+
+def breed=(breed = "Mutt")
+@breed = breed
+end
+
+#getter methods
+def name
+  @name
+end
+def breed
+  @breed
+end
 end
